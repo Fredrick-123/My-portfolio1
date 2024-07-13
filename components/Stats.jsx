20b1bccf38948031904573ebb33dbb85903524c4
Fredrick-123 <fredricknicholas8@gmail.com>
2024-07-13 14:51:14 +0100

    second commit

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -30,7 +30,7 @@ const Stats = () => {
       <div className="container mx-auto">
               <div className="flex flex-wrap gap-6 max-w-[100vw] mx-auto xl:max-w-none">
                 { stats.map((item , index) =>{
-                  return <div className="flex-1 flex  gap-2 xl:gap-4 items-center justify-center xl:justify-start" key={index}>
+                  return <div className="flex-1 flex gap-[5px] xl:gap-4 items-center justify-center xl:justify-start" key={index}>
                         <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold"/>
                         <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]" } leading-snug text-white/80`}>{item.text}</p>
                   </div>
